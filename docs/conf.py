8bdab37e96ffdc445d683b70229a21adbde07a7e
dli <terryinzaghi@163.com>
2018-12-20 05:57:55 +0000

    remove old build

--- docs/conf.py
+++ docs/conf.py
@@ -61,6 +61,8 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
+    'sphinx.ext.inheritance_diagram',
+    'autoapi.sphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
