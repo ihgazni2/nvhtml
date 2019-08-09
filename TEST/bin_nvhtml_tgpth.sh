9131f2f85af40b0ad5dd28fd8f0d54223b4bd723
dli <ihgazni2010@hotmail.com>
2019-08-09 09:50:06 +0000

    remove old build

---
+++ TEST/bin_nvhtml_tgpth.sh
@@ -0,0 +1,33 @@
+nvhtml_tgpth -input opis.html  -tgpth .
+nvhtml_tgpth -input opis.html  -tgpth htm   
+nvhtml_tgpth -input opis.html  -tgpth html.
+nvhtml_tgpth -input opis.html  -tgpth html
+nvhtml_tgpth -input opis.html  -tgpth .html
+
+
+
+nvhtml_tgpth -input opis.html  -tgpth html.b
+nvhtml_tgpth -input opis.html  -tgpth html.h
+nvhtml_tgpth -input opis.html  -tgpth .html.b
+nvhtml_tgpth -input opis.html  -tgpth .html.h
+
+
+
+nvhtml_tgpth -input opis.html  -tgpth html.body.div  
+nvhtml_tgpth -input opis.html  -tgpth html.body.div.
+
+
+
+
+
+nvhtml_tgpth -input opis.html  -tgpth html.body.div.0
+nvhtml_tgpth -input opis.html  -tgpth html.body.div.1
+nvhtml_tgpth -input opis.html  -tgpth html.body.div.2
+nvhtml_tgpth -input opis.html  -tgpth html.body.div.3
+
+nvhtml_tgpth -input opis.html  -tgpth html.body.div.form
+
+
+
+
+
---
+++ TEST/bin_nvhtml_tgpth.tst
@@ -0,0 +1,19 @@
+from lxml.etree import HTML as LXHTML
+from lxml.etree import XML as LXML
+from xdict.jprint import pdir,pobj
+from nvhtml import txt
+from nvhtml import lvsrch
+from nvhtml import fs
+from nvhtml import engine
+from nvhtml import utils
+import lxml.sax
+import argparse
+from efdir import fs
+import elist.elist as elel
+import estring.estring as eses
+import spaint.spaint as spaint
+
+
+html_str = fs.rfile("opis.html")
+root = LXHTML(html_str)
+
