e41aed757190763c938cd069d13cd9359eab79fe
dli <ihgazni2010@hotmail.com>
2019-08-19 12:24:39 +0000

    remove old build

--- nvhtml/dmat_writer.py
+++ nvhtml/dmat_writer.py
@@ -12,7 +12,7 @@ def fill_nd(nd,ele):
     nd.tail = ele['tail']
     return((nd,ele))
 
-def fill_dmat_child_nd(ele,dmat):
+def fill_dmat_child_nd(ele,dmat,comment_as_normal=True):
     chlocs = ele['children']
     pnd = ele['nd']
     for i in range(len(chlocs)):
@@ -20,27 +20,34 @@ def fill_dmat_child_nd(ele,dmat):
         chele = dmat[loc[0]][loc[1]]
         chtag = chele['tag']
         if(chtag  == "<comment>"):
-            comment = lxml.etree.Comment(chele['text'])
-            pnd.insert(i,comment)
-            chnd = engine.child(pnd,i)
+            chtag = "@comment@"
+        else:
+            pass
+        if(not(comment_as_normal)):
+            if(chtag  == "<comment>"):
+                comment = lxml.etree.Comment(chele['text'])
+                pnd.insert(i,comment)
+                chnd = engine.child(pnd,i)
+            else:
+                chnd = lxml.etree.SubElement(pnd,chtag)
         else:
             chnd = lxml.etree.SubElement(pnd,chtag)
         chele['nd'] = chnd
 
 
-def creat_root_nd(root_ele,dmat):
+def creat_root_nd(root_ele,dmat,comment_as_normal=True):
     tag = root_ele['tag']
     root = lxml.etree.Element(tag)
     root,root_ele = fill_nd(root,root_ele)
     root_ele['nd'] = root
-    fill_dmat_child_nd(root_ele,dmat)
+    fill_dmat_child_nd(root_ele,dmat,comment_as_normal=comment_as_normal)
     return(root)
 
-def dmat2etree(dmat):
+def dmat2etree(dmat,comment_as_normal=True):
     lngth = len(dmat)
     ######
     root_ele = dmat[0][0]
-    root = creat_root_nd(root_ele,dmat)
+    root = creat_root_nd(root_ele,dmat,comment_as_normal=comment_as_normal)
     ######
     for i in range(1,lngth):
         layer = dmat[i]
@@ -48,10 +55,16 @@ def dmat2etree(dmat):
             ele = layer[j]
             nd = ele['nd']
             nd,ele = fill_nd(nd,ele)
-            fill_dmat_child_nd(ele,dmat)
+            fill_dmat_child_nd(ele,dmat,comment_as_normal=comment_as_normal)
     ######
     return(root)
 
-def dmat2html(dmat):
-    r = dmat2etree(dmat)
-    return(engine.beautify(r))
+def dmat2html(dmat,comment_as_normal=True):
+    r = dmat2etree(dmat,comment_as_normal=comment_as_normal)
+    if(comment_as_normal):
+        html_str = engine.beautify(r)
+        html_str = html_str.replace("<@comment@>","<!--")
+        html_str = html_str.replace("</@comment@>","-->")
+    else:
+        html_str = lxml.etree.tostring(r).decode('utf-8')
+    return(html_str)
