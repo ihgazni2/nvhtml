a2fe746e92aeec00f52369ebd511a4ae97ad7551
dli <ihgazni2010@hotmail.com>
2019-08-19 07:41:30 +0000

    remove old build

--- nvhtml/__init__.py
+++ nvhtml/__init__.py
@@ -1,2 +1,2 @@
-__all__ = ['engine','lvsrch','txt','utils','fs','consts','htmldb','struct_show','dmat_writer']
+__all__ = ['engine','lvsrch','txt','utils','fs','consts','htmldb','struct_show','dmat_writer','rshtml']
 
