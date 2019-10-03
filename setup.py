fbff4b3edb14d63e02669c23e0137e893e582f10
dli <terryinzaghi@163.com>
2019-10-03 12:59:48 +0000

    remove old build

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup_requirements = [
 
 setup(
     name='nvhtml',
-    version='0.0.37',
+    version='0.0.38',
     description="A Python library manipulate html",
     long_description=readme + '\n\n' + installation + '\n\n' + usage + '\n\n' + history,
     author="dli",
